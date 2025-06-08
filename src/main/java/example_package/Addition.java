4a6135bfd59545d156566ced0e74cb6c221cfb6b
Gayathri190598 <rgayu1998@gmail.com>
2025-06-08 19:47:25 +0530

    initial commit

---
+++ src/main/java/example_package/Addition.java
@@ -0,0 +1,20 @@
+package example_package;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=5,b=3,sum,sub,mul,div;
+		sum=a+b;
+		System.out.println(sum);
+		sub=a-b;
+		System.out.println(sub);
+		mul=a*b;
+		System.out.println(mul);
+		div=a/b;
+		System.out.println(div);
+
+	}
+
+}
